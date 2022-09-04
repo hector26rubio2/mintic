8b68067fcc887509caaecf2da8186fb2865d6f4e
LuisCBio <luis.caro@javeriana.edu.co>
2022-09-04 17:16:59 -0500

    Empresa.cs

---
+++ veterinaria.App.Dominio/Empresa.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+namespace veterinaria.App.Dominio
+{
+    public class Empresa 
+    {
+        private Linkedlist <Veterinario> veterinarios {get; set;}
+
+        private LinkedList <Cliente> clientes {get; set;}
+
+    }
+
+}
